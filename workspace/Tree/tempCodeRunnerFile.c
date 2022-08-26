65405e8d8b94038715004e8be864f127ff76d4d6
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2022-08-26 15:36:07 +0530

    Delete workspace directory

--- workspace/Tree/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
- while(front!=NULL && rear!=NULL){
-        struct Bstnode* current=Front();
-        printf("%d",current->data);
-        if(current->left!=NULL) Push(current->left->data);
-        if(current->right!=NULL) Push(current->right->data);
-        Pop();
-
-
-   }
