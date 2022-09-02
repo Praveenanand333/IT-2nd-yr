9d52a0f2cf01e3977d48d45d869b3125715ccd81
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2022-09-02 13:43:40 +0530

    Add files via upload

---
+++ c/BASICS/delay.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+void delay(){
+int c,d;
+ for (c = 1; c <= 32760; c++){
+       for (d = 1; d <= 32760; d++)
+       {}
+       }
+
+}
+int main()
+{
+   int c, d;
+   
+       delay();
+   printf("###### ######   ##\n");
+   delay();
+   printf("#    # #    #  #  #\n");
+      delay();
+   printf("###### ###### ######\n");
+      delay();
+   printf("#      # #    #    #\n");
+      delay();
+   printf("#      #  #   #    #\n");
+      delay();
+   printf("#      #   #  #    #");
+  
+   return 0;
+}
