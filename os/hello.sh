4910a45ca744e69687686e5cfb063666c84f9e0e
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-01-27 11:38:49 +0530

    Update hello.sh

--- os/hello.sh
+++ os/hello.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+#use bash fiename.sh to run the file
 name="anand"
 echo "hello $name"
 echo $name
 echo "First Parameter : $1"
 echo "Second Parameter : $2"
 echo "Total Number of Parameters : $#"
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "${NAME[1]}"
+#for accessing the entire array
+echo "First Method: ${NAME[*]}"
+echo "Second Method: ${NAME[@]}"
+val[0]=`expr 2 \* 5`
+val[1]=`expr 2 + 5`
+val[2]=`expr 2 - 5`
+val[3]=`expr 2 / 5`
+echo "Total value : ${val[*]}"
