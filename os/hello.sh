3a2eb3872c42c56e07e842177d32e31af0c518f5
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-01-27 11:47:35 +0530

    Update hello.sh

--- os/hello.sh
+++ os/hello.sh
@@ -20,3 +20,41 @@ val[1]=`expr 2 + 5`
 val[2]=`expr 2 - 5`
 val[3]=`expr 2 / 5`
 echo "Total value : ${val[*]}"
+a=10
+b=5
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+else
+   echo "a is not equal to b"
+fi
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+fi
+
+if [ $a != $b ]
+then
+   echo "a is not equal to b"
+fi
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
+
+FRUIT="kiwi"
+
+case "$FRUIT" in "apple") echo "Apple pie is quite tasty." ;;
+   "banana") echo "I like banana nut bread." ;;
+   "kiwi") echo "New Zealand is famous for kiwi." ;;
+esac
