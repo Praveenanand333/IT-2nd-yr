45d4585f823251ab5dad7f029676ae59f8132f0f
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-08 18:59:50 +0530

    Update account.cpp

--- OOPS-and-ADS/Classes/account.cpp
+++ OOPS-and-ADS/Classes/account.cpp
@@ -25,7 +25,7 @@ int debit(int amount){
 if(acc_bal-amount>1000){
 acc_bal-=amount;}
 else{
-cout<<"Cant debit must have minimum balance of 1000";
+cout<<"\nCant debit must have minimum balance of 1000";
 }
 return acc_bal;
 }
@@ -38,16 +38,33 @@ return acc_bal;
 };
 
 int main(){
-int am;
+int am,bal;
 account a1(20000);
-int bal=a1.getbal();
-cout<<"initial sbalance is "<<bal;
+int choice;
+cout<<"\nEnter the choice 1-get balance\n 2-Credit amount \n3-Debit amount \n4-Exit\n";
+cin>>choice;
+while(choice!=4){
+    if(choice==1){
+bal=a1.getbal();
+cout<<"Balance is "<<bal;
+    }
+    if(choice==2){
 cout<<"\n Enter the amount you want to credit:";
 cin>>am;
 bal=a1.credit(am);
 cout<<"\n The updated balance is:"<<bal;
+    }
+    if(choice==3){
 cout<<"\nEnter the amount you want to debit:";
 cin>>am;
 bal=a1.debit(am);
 cout<<"\nThe updated balance is:"<<bal;
+    }
+    if(choice==4)break;
+    cout<<"\nEnter the choice 1-get balance\n 2-Credit amount \n3-Debit amount \n4-Exit\n";
+cin>>choice;
+}
+
+
+
 }
