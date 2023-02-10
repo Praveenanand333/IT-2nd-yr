591db04d9bbea209b66e821ab7bb3fe7ccf06a42
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-10 10:00:03 +0530

    Update hello.sh

--- os/hello.sh
+++ os/hello.sh
@@ -60,3 +60,6 @@ case "$FRUIT" in "apple") echo "Apple pie is quite tasty." ;;
    "banana") echo "I like banana nut bread." ;;
    "kiwi") echo "New Zealand is famous for kiwi." ;;
 esac
+c=0
+let c=$c+1
+echo $c
