e659f1a73b677ee1b0756dcfde75b6748dd549db
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-13 21:08:47 +0530

    Update student.cpp

--- OOPS-and-ADS/Classes/student.cpp
+++ OOPS-and-ADS/Classes/student.cpp
@@ -7,25 +7,12 @@ int age;
 int marks;
 
 public:
-void set_age(int age1){
-age=age1;
-}
+void set_age(int age1){age=age1;}
+void set_name(string name1){name=name1;}
+void set_marks(int marks1){marks=marks1;}
 
-void set_name(string name1){
-name=name1;
-}
-
-void set_marks(int marks1){
-marks=marks1;
-}
-
-string get_name(){
-return name;
-}
-
-int get_marks(){
-return marks;
-}
+string get_name(){return name;}
+int get_marks(){return marks;}
 };
 
 int main(){
@@ -36,16 +23,11 @@ int marks[5]={99,94,96,100,91};
 for(int i=0;i<5;i++){
 s[i].set_name(name[i]);
 s[i].set_age(age[i]);
-s[i].set_marks(marks[i]);
-
-}
+s[i].set_marks(marks[i]);}
 sort(marks,marks+5);
 cout<<"The students who got the top 3 marks are"<<endl;
 for(int i=4;i>1;i--){
 for(int j=0;j<5;j++){
 if(marks[i]==s[j].get_marks()){
 cout<<s[j].get_name()<<endl;
-}
-}
-}
-}
+}}}}
