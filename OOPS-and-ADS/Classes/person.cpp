1b906e21e7b7c0c996e6772652a0f44d9c3dea8f
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-13 21:08:13 +0530

    Update person.cpp

--- OOPS-and-ADS/Classes/person.cpp
+++ OOPS-and-ADS/Classes/person.cpp
@@ -8,38 +8,15 @@ int id;
 int salary;
 
 public:
-void set_name(string name1){
-name=name1;
-}
+void set_name(string name1){name=name1;}
+void set_age(int age1){age=age1;}
+void set_id(int id1){id=id1;}
+void set_salary(int salary1){salary=salary1;}
 
-void set_age(int age1){
-age=age1;
-}
-
-void set_id(int id1){
-id=id1;
-}
-
-void set_salary(int salary1){
-salary=salary1;
-}
-
-string get_name(){
-return name;
-}
-
-int get_age(){
-return age;
-}
-
-int get_id(){
-return id;
-}
-
-int get_salary(){
-return salary;
-}
-
+string get_name(){return name;}
+int get_age(){return age;}
+int get_id(){return id;}
+int get_salary(){return salary;}
 };
 
 int main(){
@@ -61,9 +38,7 @@ if(p[i].get_salary()>maxi){
 maxi=p[i].get_salary();
 h=p[i];
 }
-
 }
 cout<<"The name of the person getting highest salary is: "<<h.get_name();
 }
-
 
