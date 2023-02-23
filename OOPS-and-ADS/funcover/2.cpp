cc37be5ea67886a6cfc744ec79236b698082a272
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-23 15:23:09 +0530

    Add files via upload

---
+++ OOPS-and-ADS/funcover/2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void func(int a,int b){
+cout<<"This is from function accepting integer parameters"<<endl;
+cout<<"The sum is:"<<a+b<<endl;
+}
+
+void func(double a,double b){
+cout<<"This is from function accepting double parameters"<<endl;
+cout<<"The sum is:"<<a+b<<endl;
+}
+
+int main(){
+func(2,5);
+func(3.14,5.1);
+}
