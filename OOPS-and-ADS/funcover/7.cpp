01d3fd0ce445c2c2fe65085093d270675f6725d8
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-23 16:30:04 +0530

    Update 7.cpp

--- OOPS-and-ADS/funcover/7.cpp
+++ OOPS-and-ADS/funcover/7.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
+template <typename T>	
 T add(T num1, T num2) {
     return (num1 + num2);
 }
-
+template <typename T1,typename T2>	
+T2 add(T1 num1, T2 num2) {
+    return (num1 + num2);
+}
 int main() {
     int result1;
     double result2;
-    // calling with int parameters
     result1 = add<int>(2, 3);
     cout << "2 + 3 = " << result1 << endl;
-    // calling with double parameters
-    result2 = add<double>(2.2, 3.3);
-    cout << "2.2 + 3.3 = " << result2 << endl;
+    result2 = add<int,double>(2, 3.3);
+    cout << "2 + 3.3 = " << result2 << endl;
 
     return 0;
 }
