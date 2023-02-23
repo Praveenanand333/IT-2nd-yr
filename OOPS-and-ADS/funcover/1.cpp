cb9b56128931d84af60ccda2f5f81f2f7558aa58
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-23 15:15:28 +0530

    Add files via upload

---
+++ OOPS-and-ADS/funcover/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void func(int a,int b,int c){
+cout<<"This is from function accepting 3 parameters"<<endl;
+cout<<"The sum of three numbers is:"<<a+b+c<<endl;
+}
+void func(int a,int b){
+cout<<"This is from function accepting 2 parameters"<<endl;
+cout<<"The sum of two numbers is:"<<a+b<<endl;
+}
+
+int main(){
+func(1,2);
+func(1,2,3);
+}
