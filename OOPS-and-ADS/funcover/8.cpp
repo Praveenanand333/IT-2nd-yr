ddf528a9b1e37d50f782e69f308720b73139977b
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-23 16:30:23 +0530

    Update 8.cpp

--- OOPS-and-ADS/funcover/8.cpp
+++ OOPS-and-ADS/funcover/8.cpp
@@ -2,21 +2,21 @@
 #include <iostream>
 using namespace std;
 
-template <typename T1,typename T2>
-T2 add(T1 num1, T2 num2) {
+template <typename T1>
+T1 add(T1 num1, T1 num2) {
     return (num1 + num2);
 }
-template <typename T3>
-T3 add(T3 num1, T3 num2) {
-    return (num1 + num2);
+template <typename T1>
+T1 add(T1 num1, T1 num2,T1 num3) {
+    return (num1 + num2+num3);
 }
 
 int main() {
     double result1;
     double result2;
-    result1 = add<int,double>(2, 3.3);
+    result1 = add<int>(2, 3);
     cout << "2 + 3.3 = " << result1 << endl;
-    result2 = add<double>(2.2, 3.3);
-    cout << "2.2 + 3.3 = " << result2 << endl;
+    result2 = add<double>(2.2, 3.3,1.1);
+    cout << "2.2 + 3.3+1.1 = " << result2 << endl;		
     return 0;
 }
