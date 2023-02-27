89a0101f487702654a9890d6fd3d5cf336678056
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-02-27 18:43:06 +0530

    final

--- OOPS-and-ADS/funcover/1.cpp
+++ OOPS-and-ADS/funcover/1.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 void func(int a,int b,int c){
 cout<<"This is from function accepting 3 parameters"<<endl;
 cout<<"The sum of three numbers is:"<<a+b+c<<endl;
@@ -9,7 +8,6 @@ void func(int a,int b){
 cout<<"This is from function accepting 2 parameters"<<endl;
 cout<<"The sum of two numbers is:"<<a+b<<endl;
 }
-
 int main(){
 func(1,2);
 func(1,2,3);
