2ede06698adc80c858a1b91bc9d2545e5c31dcf4
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-05-24 18:19:49 +0530

    Update topologicalsort.cpp

--- OOPS-and-ADS/Graph-Traversal/topologicalsort.cpp
+++ OOPS-and-ADS/Graph-Traversal/topologicalsort.cpp
@@ -1,60 +1,82 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-class Graph {
-    int V;  
-    list<int> *adj; 
-    void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
-public:
-    Graph(int V);  
-    void addEdge(int v, int w);  
-    void topologicalSort();  
-};
+// Function to perform topological sort
+vector<int> topologicalSort(vector<vector<int>>& graph, int numCourses) {
+    vector<int> inDegree(numCourses, 0);
+    vector<int> result;
+    queue<int> q;
 
-Graph::Graph(int V) {
-    this->V = V;
-    adj = new list<int>[V];
-}
-
-void Graph::addEdge(int v, int w) {
-    adj[v].push_back(w);
-}
-
-void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack) {
-    visited[v] = true;
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
-            topologicalSortUtil(*i, visited, Stack);
-    Stack.push(v);
-}
-
-void Graph::topologicalSort() {
-    stack<int> Stack;
-    bool *visited = new bool[V];
-    for (int i = 0; i < V; i++)
-        visited[i] = false;
-    for (int i = 0; i < V; i++)
-        if (visited[i] == false)
-            topologicalSortUtil(i, visited, Stack);
-    while (Stack.empty() == false) {
-        cout << Stack.top() << " ";
-        Stack.pop();
+    // Calculate the in-degree of each vertex
+    for (int i = 0; i < numCourses; i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            int course = graph[i][j];
+            inDegree[course]++;
+        }
     }
+
+    // Enqueue vertices with in-degree 0
+    for (int i = 0; i < numCourses; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    // Perform topological sort
+    while (!q.empty()) {
+        int course = q.front();
+        q.pop();
+        result.push_back(course);
+
+        // Reduce the in-degree of adjacent vertices
+        for (int i = 0; i < graph[course].size(); i++) {
+            int adjacentCourse = graph[course][i];
+            inDegree[adjacentCourse]--;
+
+            // Enqueue vertices with in-degree 0
+            if (inDegree[adjacentCourse] == 0) {
+                q.push(adjacentCourse);
+            }
+        }
+    }
+
+    // If not all vertices are included, there is a cycle in the graph
+    if (result.size() != numCourses) {
+        result.clear(); // Clear the result to indicate failure (cycle detected)
+    }
+
+    return result;
 }
 
 int main() {
-    Graph g(6);
-    g.addEdge(5, 2);
-    g.addEdge(5, 0);
-    g.addEdge(4, 0);
-    g.addEdge(4, 1);
-    g.addEdge(4, 2);
-    g.addEdge(2, 3);
-    g.addEdge(3, 1);
+    int numCourses = 6;
+    vector<vector<int>> graph(numCourses);
 
-    cout << "Topological Sort: ";
-    g.topologicalSort();
+    // Add edges to the graph
+    graph[5].push_back(2);
+    graph[5].push_back(0);
+    graph[4].push_back(0);
+    graph[4].push_back(1);
+    graph[4].push_back(2);
+    graph[2].push_back(3);
+    graph[3].push_back(1);
+
+    // Perform topological sort
+    vector<int> sortedCourses = topologicalSort(graph, numCourses);
+
+    // Print the result
+    if (sortedCourses.empty()) {
+        cout << "Cycle detected. Topological sort is not possible." << endl;
+    } else {
+        cout << "Topological Sort: ";
+        for (int i = 0; i < sortedCourses.size(); i++) {
+            cout << sortedCourses[i] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
