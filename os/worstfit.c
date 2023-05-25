e86a3846f5d349edfedb44da21be58a445df33e6
Praveen <104721138+Praveenanand333@users.noreply.github.com>
2023-05-25 20:50:21 +0530

    Add files via upload

---
+++ os/worstfit.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#define MEMORY_SIZE 1000
+#define MAX_PROCESSES 100
+
+struct Process {
+    int process_id;
+    int memory_size;
+    int allocated_block;
+};
+
+void worstFit(struct Process processes[], int num_processes, int blocks[], int num_blocks) {
+    int allocation[num_processes];
+
+    for (int i = 0; i < num_processes; i++) {
+        int worst_index = -1;
+        for (int j = 0; j < num_blocks; j++) {
+            if (blocks[j] >= processes[i].memory_size) {
+                if (worst_index == -1 || blocks[j] > blocks[worst_index]) {
+                    worst_index = j;
+                }
+            }
+        }
+
+        if (worst_index != -1) {
+            allocation[i] = worst_index;
+            blocks[worst_index] -= processes[i].memory_size;
+        } else {
+            allocation[i] = -1;
+        }
+    }
+
+    printf("\nProcess ID\tProcess Size\tBlock Number\n");
+    for (int i = 0; i < num_processes; i++) {
+        printf("%d\t\t%d\t\t", processes[i].process_id, processes[i].memory_size);
+        if (allocation[i] != -1) {
+            printf("%d\n", allocation[i]);
+        } else {
+            printf("Not Allocated\n");
+        }
+    }
+}
+
+int main() {
+    int num_processes, num_blocks;
+    printf("Enter the number of processes: ");
+    scanf("%d", &num_processes);
+
+    struct Process processes[MAX_PROCESSES];
+    printf("Enter the details of processes:\n");
+    for (int i = 0; i < num_processes; i++) {
+        printf("Process ID: ");
+        scanf("%d", &processes[i].process_id);
+        printf("Memory Size: ");
+        scanf("%d", &processes[i].memory_size);
+        processes[i].allocated_block = -1;
+    }
+
+    printf("\nEnter the number of memory blocks: ");
+    scanf("%d", &num_blocks);
+
+    int blocks[num_blocks];
+    printf("Enter the details of memory blocks:\n");
+    for (int i = 0; i < num_blocks; i++) {
+        printf("Block %d Size: ", i);
+        scanf("%d", &blocks[i]);
+    }
+
+    worstFit(processes, num_processes, blocks, num_blocks);
+
+    return 0;
+}
